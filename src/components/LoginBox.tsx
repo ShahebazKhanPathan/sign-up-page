abc48a66fff3a55d10ef4db7d3206382ecad3d4b
ShahebazKhanPathan <pathanshahebazk@gmail.com>
2023-12-29 18:47:34 +0530

    Create functionality

--- src/components/LoginBox.tsx
+++ src/components/LoginBox.tsx
@@ -1,12 +1,42 @@
-import { Box, HStack } from "@chakra-ui/react";
-import LoginForm from "./LoginForm";
+import { Alert, AlertIcon, AlertTitle, Box, HStack, Spinner } from "@chakra-ui/react";
+import LoginForm, { User } from "./LoginForm";
+import axios from "axios";
+import { useState } from "react";
 
 const LoginBox = () => {
+    const [response, setResponse] = useState(false);
+    const [error, setError] = useState('');
+    const [isLoading, setLoading] = useState(false);
+
+    const createUser = (data: User) => {
+        setLoading(true);
+        axios.post('http://localhost:3000/api/users', { data })
+            .then(() => {
+                setLoading(false);
+                setResponse(true);
+                setTimeout(()=> setResponse(false), 5000)
+            })
+            .catch((err) => {
+                setLoading(false);
+                setError(err.message);
+                setTimeout(() => setError(''), 5000)
+            });
+    }
+
     return (
         <HStack justifyContent="space-between">
             <Box></Box>
             <Box padding={5} border="1px" borderColor="white">
-                <LoginForm/>
+                {error && <Alert status='error'>
+                    <AlertIcon />
+                    <AlertTitle>{error}</AlertTitle>
+                </Alert>}
+                {response && <Alert status='success'>
+                    <AlertIcon />
+                    Data uploaded to the server!
+                </Alert>}
+                {isLoading && <Spinner/>}
+                <LoginForm onSubmit={(data) => createUser(data)}/>
             </Box>
             <Box></Box>
         </HStack>
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,17 +1,36 @@
-import { Button, FormControl, FormLabel, Input, Heading } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
 
-const LoginForm = () => {
+export interface User {
+    name: string;
+    email: string;
+    password: string;
+}
+
+interface Props {
+    onSubmit: (data: User) => void;
+}
+
+const LoginForm = ({ onSubmit }: Props) => {
+
+    const { register, handleSubmit, reset } = useForm<User>();
+
     return (
-        <FormControl>
-            <Heading mb={3}>Sign Up</Heading>
-            <FormLabel>Name</FormLabel>
-            <Input type="text" />
-            <FormLabel>Email</FormLabel>
-            <Input type="email" />
-            <FormLabel>Password</FormLabel>
-            <Input type="password" />
-            <Button mt={3} colorScheme="blue">Submit</Button>
-        </FormControl>
+        <form onSubmit={handleSubmit((data) => { onSubmit(data);  reset({ name: "", email: "", password: ""})  })}>
+            <h3>Sign Up</h3>
+            <div className="form-group">
+                <div className="label form-label">Name</div>
+                <input {...register('name', { minLength: 5, required: true })} type="text" id="name" className="form-control" />
+            </div>
+            <div className="form-group">
+                <div className="label form-label">Email</div>
+                <input {...register('email', { minLength: 5, required: true})} type="email" id="email" className="form-control" />
+            </div>
+            <div className="form-group mb-3">
+                <div className="label form-label">Password</div>
+                <input {...register('password', { minLength: 8, required: true})} type="password" className="form-control" />
+            </div>
+            <button className="btn btn-primary" type="submit">Submit</button>
+        </form> 
     )
 }
 
